1d2d55758c3f05f69ac11799bbb292e8e7d5b134
José Ulombe <jose.ulombe@hotmail.com>
2020-10-13 19:10:10 +0100

    chore: update scripts

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -8,7 +8,24 @@ L.tileLayer(
 
 
 // create icon
+const icon = L.icon({
+   iconUrl: "./public/images/map-marker.svg",
+   iconSize: [58, 68],
+   iconAnchor: [29, 68],
+   popupAnchor: [170, 2]
+})
 
-L.marker([-8.9055183,13.2221317]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup()
+
+// create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+
+// create and add marker
+L.marker([-8.9055183,13.2221317], { icon })
+.addTo(map)
+.bindPopup(popup)
+    
