144922c84d5eb63ae23fad721e7c8827911924a5
José Ulombe <jose.ulombe@hotmail.com>
2020-10-19 01:25:08 +0100

    day 5

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -8,10 +8,14 @@ const options = {
     zoomControl:false
 }
 
+// get values from html
+const lat = document.querySelector('span[data-lat]').dataset.lat
+const lng = document.querySelector('span[data-lng]').dataset.lng
+
 
 
 // create map
-const map = L.map('mapid', options).setView([-8.9055183,13.2221317], 15)
+const map = L.map('mapid', options).setView([lat,lng], 15)
 
 
 // create and add tileLayer
@@ -28,7 +32,9 @@ const icon = L.icon({
 })
 
 // create and add marker
-L.marker([-8.9055183,13.2221317], { icon })
+
+
+L.marker([lat,lng], { icon })
 .addTo(map)
 
 
